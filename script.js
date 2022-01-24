383adf7e4b83b3eff4a717f70ed31b151fd44ea0
Apvivar92@gmail.com <Apvivar92@gmail.com>
2022-01-23 22:42:46 -0800

    Fixed button animation in JS

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 /* Create event to activate sidebar when clicked */
 document.getElementById('myBtn').addEventListener('click', open_close);
 
+var hamburgerMenu = document.getElementById('hamburger');
 /*Detemines status of the side nav bar*/
-var menuState = 1; //closed
+var menuState = 0; //closed
 /* create the open and close function*/
 function open_close() {
   if (menuState === 0) {
@@ -11,9 +12,18 @@ function open_close() {
     /*make sidebbar move right 250px */
     document.getElementById('mySidebar').style.width = '250px';
     document.getElementById('main').style.marginLeft = '250px';
+
+    hamburgerMenu.classList.add('hamburgerActive');
   } else {
     menuState = 0; /*close sidebar*/
     document.getElementById('mySidebar').style.width = '0';
     document.getElementById('main').style.marginLeft = '0';
+
+    hamburgerMenu.classList.remove('hamburgerActive');
   }
 }
+
+// function myFunction() {
+//   var element = document.getElementById('myBtn');
+//   element.classList.add('hamburger');
+// }
